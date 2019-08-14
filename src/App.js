85e75ee4e4d0cd6d3f4cfd11c231fee8dd817887
Cesar Cachay <cesarcachay1996@gmail.com>
2019-08-13 19:05:57 -0500

    Add theme provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Navbar from "./components/Navbar";
 import Form from "./components/Form";
-import PageContent from "./components/PageContent";
 
-function App() {
-  return (
-    <PageContent>
-      <Navbar />
-      <Form />
-    </PageContent>
-  );
+import PageContent from "./components/PageContent";
+import { ThemeProvider } from "./contexts/ThemeContext";
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider>
+        <PageContent>
+          <Navbar />
+          <Form />
+        </PageContent>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
